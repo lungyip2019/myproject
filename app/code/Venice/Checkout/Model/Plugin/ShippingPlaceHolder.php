812dfbd97a08380e5acbacfe0a0b7adfb944eb70
Lung Yip <lungyip@targa.com>
2018-08-01 14:26:14 +0800

    update 0801
    update

--- app/code/Venice/Checkout/Model/Plugin/ShippingPlaceHolder.php
+++ app/code/Venice/Checkout/Model/Plugin/ShippingPlaceHolder.php
@@ -3,7 +3,7 @@ namespace Venice\Checkout\Model\Plugin;
 
 use Magento\Checkout\Block\Checkout\AttributeMerger;
 
-// Todo add the placehoder to the address.street
+// Todo add the placehoder to the checkout
 
 class ShippingPlaceHolder
 {
@@ -12,6 +12,30 @@ class ShippingPlaceHolder
         if (array_key_exists('street', $result)) {
             $result['street']['children'][0]['label'] = __('Street Address*');
             $result['street']['children'][1]['label'] = __('Street Address 2');
+        }
+
+        if (array_key_exists('firstname', $result)) {
+            $result['firstname']['label'] = __('First Name*');
+        }
+
+        if (array_key_exists('lastname', $result)) {
+            $result['lastname']['label'] = __('Last Name*');
+        }
+
+        if (array_key_exists('telephone', $result)) {
+            $result['telephone']['label'] = __('Phone Number*');
+        }
+
+        if (array_key_exists('city', $result)) {
+            $result['city']['label'] = __('City*');
+        }
+
+        if (array_key_exists('region_id', $result)) {
+            $result['region_id']['label'] = __('State/Province*');
+        }
+
+        if (array_key_exists('postcode', $result)) {
+            $result['postcode']['label'] = __('Zip/Postal Code*');
         }
 
         return $result;
